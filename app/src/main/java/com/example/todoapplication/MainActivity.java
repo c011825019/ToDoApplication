7e72be731d95c3f4dcc9defe9ffefad9eb5c3790
c011825019 <c011825019@gmail.com>
2023-02-24 18:08:20 +0900

    サブメニュー追加

--- app/src/main/java/com/example/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/todoapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.todoapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String message = item.getTitle() + "が押されました";
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
