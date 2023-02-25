0b3ea0f4484d6fe3cd3339e23bf3ae31fd9137e8
c011825019 <c011825019@gmail.com>
2023-02-25 15:27:12 +0900

    AddTaskActivity 画面遷移

--- app/src/main/java/com/example/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/todoapplication/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent;
